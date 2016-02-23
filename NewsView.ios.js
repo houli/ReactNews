36cd320aca71f6f89f0a0dba71f3f829de257d71
Eoin Houlihan <ehoulih@tcd.ie>
2016-02-23 17:29:56 +0000

    Working mews

---
+++ NewsView.ios.js
@@ -0,0 +1,110 @@
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  WebView
+} from 'react-native';
+
+import TableView, {
+  Item,
+  Section
+} from 'react-native-tableview';
+
+const TOP_URL = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+const ITEM_URL = 'https://hacker-news.firebaseio.com/v0/item/';
+
+class NewsView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      newsItems: []
+    };
+    this.fetchTop();
+  }
+
+  componentDidMount() {
+    this.fetchTop();
+  }
+
+  fetchTop() {
+    fetch(TOP_URL)
+    .then(response => response.json())
+    .then(responseData => {
+      let promises = responseData.slice(0, 20).map(item => {
+        return fetch(`${ITEM_URL}${item}.json`).then(response => response.json());
+      });
+      Promise.all(promises).then(arr => {
+        this.setState({
+          newsItems: arr,
+          loaded: true
+        });
+      }, reason => {
+        console.log(reason);
+      });
+    })
+    .done();
+  }
+
+  onRowPressed(item) {
+    this.props.navigator.push({
+      title: item.title,
+      component: WebView,
+      passProps: {
+        source: {
+          uri: item.url
+        },
+        style: styles.webview,
+        startInLoadingState: true,
+        scalesPagesToFit: true
+      }
+    });
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.loading}>
+          Loading news...
+        </Text>
+      </View>
+    );
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    } else {
+      let items = this.state.newsItems.map((item) => {
+        return <Item key={item.id} onPress={() => this.onRowPressed(item)}>{item.title}</Item>;
+      });
+      return (
+        <TableView style={styles.container}
+                   allowsMultipleSelection={true}
+                   tableViewStyle={TableView.Consts.Style.Grouped}
+                   tableViewCellStyle={TableView.Consts.CellStyle.Subtitle}>
+          <Section>
+            {items}
+          </Section>
+        </TableView>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 32,
+    marginBottom: 8
+  }, loading: {
+    flex: 1,
+    paddingTop: 32,
+    fontSize: 30
+  }, webview: {
+    flex: 1
+  }
+});
+
+export default NewsView;
--- index.ios.js
+++ index.ios.js
@@ -6,47 +6,32 @@
 import React, {
   AppRegistry,
   Component,
-  StyleSheet,
-  Text,
-  View
+  NavigatorIOS,
+  StyleSheet
 } from 'react-native';
+
+import NewsView from './NewsView';
 
 class ReactNews extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.navigation}
+        barTintColor='#ff6600'
+        titleTextColor='white'
+        initialRoute={{
+          component: NewsView,
+          title: 'Hacker News'
+        }}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  navigation: {
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('ReactNews', () => ReactNews);
